425ef24539a64f5246fdd9bcdbeb254375e6d7b4
DanielCG <danielcgilibert@gmail.com>
2023-07-05 20:33:25 +0200

    🔧 chore(astro.config.mjs): reformat code for better readability and consistency ✨ feat(astro.config.mjs): add tailwind integration to the project for improved styling capabilities 🔧 chore(astro.config.mjs): update markdown configuration to use the 'github-dark-dimmed' theme and enable line wrapping for better code readability 🔧 chore(astro.config.mjs): update integrations to include mdx, sitemap, and tailwind for enhanced functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://example.com',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      wrap: true,
+    },
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
+})
