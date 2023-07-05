36238a7b593438e3d15d7db25f8a4472079098f4
DanielCG <danielcgilibert@gmail.com>
2023-07-05 20:34:38 +0200

    📝 chore(tailwind.config.cjs): add tailwind configuration file
    The tailwind.config.cjs file is added to the project. This file contains the configuration for Tailwind CSS. It specifies the content to be processed by Tailwind CSS, extends the default theme with customizations, and adds the 'manrope' font family to the sans font stack. Additionally, the '@tailwindcss/typography' plugin is included.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['manrope', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
