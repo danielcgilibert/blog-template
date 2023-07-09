b92811b9dfc0ba1505b3f8d9e78cc9b971a032d2
DanielCG <danielcgilibert@gmail.com>
2023-07-09 17:35:00 +0200

    🔧 chore(astro.config.mjs): update astro.config.mjs to include remark-toc plugin for generating table of contents in markdown files 🚀 feat(package.json): add remark-toc as a dependency to enable table of contents generation in markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,31 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
+import remarkToc from 'remark-toc'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   markdown: {
+    remarkPlugins: [remarkToc],
+    // drafts: true,
     shikiConfig: {
       theme: 'github-dark-dimmed',
       wrap: true,
     },
   },
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'github-dark-dimmed',
+        wrap: true,
+      },
+      remarkPlugins: [remarkToc],
+      // drafts: true,
+    }),
+    sitemap(),
+    tailwind(),
+  ],
 })
