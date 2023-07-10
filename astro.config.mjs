d4e56f7108da6467382df818b34a22da143d4f1b
DanielCG <danielcgilibert@gmail.com>
2023-07-10 13:26:58 +0200

    🔧 chore(astro.config.mjs): fix formatting and add missing commas for better code readability ✨ feat(astro.config.mjs): update site URL to 'https://blog-template-gray.vercel.app/' for correct website configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import remarkToc from 'remark-toc';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import remarkToc from 'remark-toc'
 
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog-template-gray.vercel.app/', // Write here your website url
   markdown: {
     remarkPlugins: [remarkToc],
     // drafts: true,
     shikiConfig: {
       theme: 'github-dark-dimmed',
-      wrap: true
-    }
-  },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'github-dark-dimmed',
-      wrap: true
+      wrap: true,
     },
-    remarkPlugins: [remarkToc]
-    // drafts: true,
-  }), sitemap(), tailwind(), image()]
-});
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'github-dark-dimmed',
+        wrap: true,
+      },
+      remarkPlugins: [remarkToc],
+      // drafts: true,
+    }),
+    sitemap(),
+    tailwind(),
+    image(),
+  ],
+})
