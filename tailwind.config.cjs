7502de4cd072b38204e2f7a41565e5e1ed686b27
Daniel <danielcgilibert@gmail.com>
2023-08-08 05:59:31 +0200

    🔧 chore(tailwind.config.cjs): reformat tailwind configuration file for better readability and consistency

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        white: '#f8f9fa',
-      },
-      fontFamily: {
-        body: ['Manrope', ...defaultTheme.fontFamily.sans],
-      },
-      gridTemplateColumns: {
-        list: 'repeat(auto-fill, minmax(400px, max-content))',
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/typography')],
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				white: '#f8f9fa'
+			},
+			fontFamily: {
+				body: ['Manrope', ...defaultTheme.fontFamily.sans]
+			},
+			gridTemplateColumns: {
+				list: 'repeat(auto-fill, minmax(400px, max-content))'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
 }
