7c2c9b0c83aff6cc12e942486f34959d4bd50942
duylv <duylv@bap.jp>
2024-06-20 10:27:31 +0700

    Fix: rss.xml not found

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,12 +2,12 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import { siteConfig } from '@/site-config'
 
-export async function get() {
+export async function GET(context: any) {
 	const posts = await getCollection('blog')
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
-		site: import.meta.env.SITE,
+		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
 			link: `post/${post.slug}/`
