ae82545e12591dd680bfa975957752b0cc43b3c8
Oded Niv <oded.niv@gmail.com>
2024-11-17 20:57:37 +0000

    use site.config for site, author, and title
    Instead of updating astro.config, Footer, index.astro,
    and site.config - just use site.config values.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
+import { siteConfig } from './src/data/site.config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	site: siteConfig.site,
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
